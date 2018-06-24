b0cf2c32aa4352e1996213009a449d608980d15a
Connor Nelson <Connor@ConnorNelson.com>
2018-06-24 15:56:24 -0700

    Initial commit

---
+++ init.el
@@ -0,0 +1,6 @@
+;; This file replaces itself with the actual configuration at first run.
+(require 'org)
+(find-file (concat user-emacs-directory "init.org"))
+(org-babel-tangle)
+(load-file (concat user-emacs-directory "init.el"))
+(byte-compile-file (concat user-emacs-directory "init.el"))
